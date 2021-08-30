b15be160062b0d99ca8cb0ab310dc8a121eaa877
DHINAKARAN <DHINAKARAN@INLTWS000025>
2021-08-30 15:19:12 +0530

    This is first project for commit

---
+++ testjk/src/testjk/testjkns.java
@@ -0,0 +1,14 @@
+package testjk;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testjkns {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "E:\\\\Selenium\\\\chromedriver\\\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+	}
+
+}
