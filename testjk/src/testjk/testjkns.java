d164097581b663522729e5322ac34338fea611ba
DHINAKARAN <DHINAKARAN@INLTWS000025>
2021-08-31 22:39:35 +0530

    this is one last commit

--- testjk/src/testjk/testjkns.java
+++ testjk/src/testjk/testjkns.java
@@ -1,18 +1,17 @@
 package testjk;  //this is sampletest done by dhinakaran for jenkins
 
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 //import org.testng.annotations.Test;
 
 //@Test
 public class testjkns {
 
 	public static void main(String[] args) {
-		System.out.println("This is my first commit message");
-
-	}
-		/*System.setProperty("webdriver.chrome.driver", "E:\\\\Selenium\\\\chromedriver\\\\chromedriver.exe");
+		//System.out.println("This is my first commit message");
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\chromedriver\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
-        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");*/
+        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
 		
 	}
+}
