755f38b34525e050ec9f1e847a55910ca5d59eb5
DHINAKARAN <DHINAKARAN@INLTWS000025>
2021-08-31 22:46:36 +0530

    commit

--- testjk/src/testjk/testjkns.java
+++ testjk/src/testjk/testjkns.java
@@ -8,10 +8,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class testjkns {
 
 	public static void main(String[] args) {
-		//System.out.println("This is my first commit message");
-		System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\chromedriver\\chromedriver.exe");
+		System.out.println("This is my first commit message");
+		/*System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\chromedriver\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
-        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");*/
 		
 	}
 }
