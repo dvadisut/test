7ddddd721c3b28ebfd0334169a203be0a7d8f9a0
DHINAKARAN <DHINAKARAN@INLTWS000025>
2021-09-01 11:00:17 +0530

    fsfsa

--- testjk/src/testjk/testjkns.java
+++ testjk/src/testjk/testjkns.java
@@ -1,4 +1,4 @@
-package testjk;  //this is sampletest done by dhinakaran for jenkins testing
+package testjk;  //this is sampletest done by dhinakaran for jenkins
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
